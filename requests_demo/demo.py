ba6ee2f6026cdceec7be0ce22a8224d96093c80c
Alex Koumparos <crossroadsman@gmail.com>
2018-05-09 15:39:37 -0600

    Update demo.py

--- requests_demo/demo.py
+++ requests_demo/demo.py
@@ -1,32 +1,36 @@
 import requests
 
+def print_details(request):
+    print("Various attributes on the request object (get):")
+    print("-----------------------------------------------")
+    print("apparent_encoding:")
+    print(request.apparent_encoding)
+    print("content:")
+    print(request.content)
+    print("text:")
+    print(request.text)
+    print("status_code:")
+    print(request.status_code)
+    print("url:")
+    print(request.url)
+
+    print("Various methods on the request object (get):")
+    print("--------------------------------------------")
+    print("iter_content():")
+    for element in request.iter_content():
+        print(element)
+    print("iter_lines():")
+    for line in request.iter_lines():
+        print(line)
+    print("json():")
+    for element in request.json():
+        print(element)
+    
+
 # ------
 
 if __name__ == "__main__":
 
     r = requests.get('https://api.github.com/events')
     print(r)
-
-    print("Various attributes on the request object (get):")
-    print("-----------------------------------------")
-    print("apparent_encoding:")
-    print(r.apparent_encoding)
-    print("content:")
-    print(r.content)
-    print("text:")
-    print(r.text)
-    print("status_code:")
-    print(r.status_code)
-    print("url:")
-    print(r.url)
-
-    print("Various methods on the request object (get):")
-    print("iter_content():")
-    for element in r.iter_content():
-        print(element)
-    print("iter_lines():")
-    for line in r.iter_lines():
-        print(line)
-    print("json():")
-    for element in r.json():
-        print(element)
+    print_details(request)
